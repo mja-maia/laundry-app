b442a580e72b7d712645bcb73961711587161d19
Miguel Maia <miguel@cupdev.com.br>
2023-03-22 22:10:04 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDF6_nwZ3S0DOqQfQmotDsK57KSaQ4MkbA",
-  authDomain: "laundry-sky.firebaseapp.com",
-  databaseURL: "https://laundry-sky.firebaseio.com",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
   projectId: "laundry-sky",
-  storageBucket: "laundry-sky.appspot.com",
-  messagingSenderId: "928149548505",
-  appId: "1:928149548505:web:a669c34c03511b072ebdc8"
+  storageBucket: "BUCKET",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
 };
 // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
